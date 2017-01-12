f4b520493f03568aa2a24693ffd549fd444af7f1
Kelly Copley <copleykj@gmail.com>
2017-01-11 22:27:51 -0500

    linter auto-fixed some shit

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
+/* eslint-disable no-undef */
 Package.describe({
-  name: 'socialize:requestable',
-  version: '1.0.0',
-  summary: 'Create models that are requestable',
-  git: 'https://github.com/copleykj/socialize-requestable.git',
+    name: 'socialize:requestable',
+    version: '1.0.0',
+    summary: 'Create models that are requestable',
+    git: 'https://github.com/copleykj/socialize-requestable.git',
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.3.4.1');
-  api.use('ecmascript');
-  api.mainModule('server/server.js', 'server');
-  api.mainModule('common/common.js');
+Package.onUse(function _(api) {
+    api.versionsFrom('1.3');
+
+    api.use('ecmascript');
+
+    api.mainModule('server/server.js', 'server');
+    api.mainModule('common/common.js');
 });
