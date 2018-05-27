cbf62d8c3e61a6c43dbeb033ed2ac034174dbda4
Kelly Copley <copleykj@gmail.com>
2018-05-26 22:45:17 -0400

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* global Package */
 Package.describe({
     name: 'socialize:requestable',
-    version: '1.0.2',
+    version: '1.0.3',
     summary: 'Create models that are requestable',
     git: 'https://github.com/copleykj/socialize-requestable.git',
 });
