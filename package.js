f75c49d5f62d00067e9c073919b784d5e7ab4898
Kelly Copley <copleykj@gmail.com>
2021-06-28 08:02:13 -0400

    update deps and versionsFrom for compatibility

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 /* global Package */
 Package.describe({
     name: 'socialize:requestable',
-    version: '1.0.5',
+    version: '1.0.6',
     summary: 'Create models that are requestable',
     git: 'https://github.com/copleykj/socialize-requestable.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.10.2');
+    api.versionsFrom(['1.10.2', '2.3']);
 
-    api.use('socialize:linkable-model@1.0.5');
+    api.use('socialize:linkable-model@1.0.6');
 
     api.mainModule('common/common.js');
 });
